bce6e633eebe7970ff80deb7561045cff895587f
Mituls-code <mitulpatel9897@gmail.com>
2021-05-07 18:15:28 +0530

    n

---
+++ java/click.js
@@ -0,0 +1,10 @@
+function img1() {
+    document.getElementById('img').src = "https://dyl80ryjxr1ke.cloudfront.net/external_assets/hero_examples/hair_beach_v1785392215/result.jpeg"
+
+}
+
+function img2() {
+    document.getElementById('img').src = "https://static.remove.bg/remove-bg-web/2a274ebbb5879d870a69caae33d94388a88e0e35/assets/start_remove-79a4598a05a77ca999df1dcb434160994b6fde2c3e9101984fb1be0f16d0a74e.png"
+
+
+}
